716a506e771626d733d1cc6ca90acbd4be22c255
Aroniaina <a.aroniaina.s.p@gmail.com>
2021-03-22 19:28:43 +0300

    Initial commit

---
+++ src/main/java/com/etech/poc/voitures/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.etech.poc.voitures.controller;
+
+/**
+ *
+ * @author a.safidy
+ */
+public class UserController {
+
+}
---
+++ src/main/java/com/etech/poc/voitures/services/applicatif/user/UserSA.java
@@ -0,0 +1,9 @@
+package com.etech.poc.voitures.services.applicatif.user;
+
+/**
+ *
+ * @author a.safidy
+ */
+public interface UserSA {
+
+}
